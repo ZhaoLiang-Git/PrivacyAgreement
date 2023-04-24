d04ce552a0b302377e467275c868df18d3fbe10b
zhaoliang <1767269406@qq.com>
2023-04-24 18:38:20 +0800

    删除 gyyxsdk/.gitignore 文件夹中的过滤/build 删除git设置中的过滤jar包

---
+++ GY_SDK_UnityWebViewPrivacyAgreementXXG/AndroidSdk/gyyxsdk/build/generated/source/buildConfig/debug/com/gyyx/androidsdk/androidsdk/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.gyyx.androidsdk.androidsdk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.gyyx.androidsdk.androidsdk";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.gyyx.androidsdk.androidsdk";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
